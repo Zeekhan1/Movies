76eccd5f16404f6795009416a3ef8524c37c7463
Zeekhan1 <53535755+Zeekhan1@users.noreply.github.com>
2020-01-07 16:04:59 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
@@ -7,23 +8,21 @@ const Movie = ({ movie }) => {
   const poster =
     movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
   return (
-    <div className="movie">
-      <h2>{movie.Title}</h2>
+    <div className="container">
       <div>
-        <img
-          width="200"
-          alt={`The movie titled: ${movie.Title}`}
-          src={poster}
-        />
+        <h2>{movie.Title}</h2>
+        <div>
+          <img
+            width="400"
+            alt={`The movie titled: ${movie.Title}`}
+            src={poster}
+          />
+        </div>
+        <p>({movie.Year})</p>
+        <Link to="/details">
+          <a className="btn btn-dark">View More</a>
+        </Link>
       </div>
-      <p>({movie.Year})</p>
-      <a
-        href=""
-        onClick={() => <Movie />}
-        className="btn btn-primary stretched-link"
-      >
-        View More
-      </a>
     </div>
   );
 };
