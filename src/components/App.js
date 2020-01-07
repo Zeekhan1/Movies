229a13941b1f9d85e8489fbd44dfa5a54504a3fd
Zeekhan1 <53535755+Zeekhan1@users.noreply.github.com>
2020-01-07 16:05:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,103 +1,18 @@
-import React, { useReducer, useEffect } from "react";
-import "../App.css";
-import Header from "./Header";
-import Movie from "./Movie";
-import Search from "./Search";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import MovieDetail from "./MovieDetail";
-
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
-
-const initialState = {
-  loading: true,
-  movies: [],
-  errorMessage: null
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SEARCH_MOVIES_REQUEST":
-      return {
-        ...state,
-        loading: true,
-        errorMessage: null
-      };
-    case "SEARCH_MOVIES_SUCCESS":
-      return {
-        ...state,
-        loading: false,
-        movies: action.payload
-      };
-    case "SEARCH_MOVIES_FAILURE":
-      return {
-        ...state,
-        loading: false,
-        errorMessage: action.error
-      };
-    default:
-      return state;
-  }
-};
+import { MovieList } from "./MovieList";
+import Movie from "./Movie";
+import "../App.css";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    fetch(MOVIE_API_URL)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        dispatch({
-          type: "SEARCH_MOVIES_SUCCESS",
-          payload: jsonResponse.Search
-        });
-      });
-  }, []);
-
-  const search = searchValue => {
-    dispatch({
-      type: "SEARCH_MOVIES_REQUEST"
-    });
-
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (jsonResponse.Response === "True") {
-          dispatch({
-            type: "SEARCH_MOVIES_SUCCESS",
-            payload: jsonResponse.Search
-          });
-        } else {
-          dispatch({
-            type: "SEARCH_MOVIES_FAILURE",
-            error: jsonResponse.Error
-          });
-        }
-      });
-  };
-
-  const { movies, errorMessage, loading } = state;
-
   return (
-    <div className="App">
-      <Header text="Search Movie here" />
-      <Search search={search} />
-      <p className="App-intro"></p>
-      <div className="movies">
-        {loading && !errorMessage ? (
-          <span>loading... </span>
-        ) : errorMessage ? (
-          <div className="errorMessage">{errorMessage}</div>
-        ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
-        )}
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={MovieList} />
+        <Route path="/details" exact component={MovieDetail} />
       </div>
-      <div className="movie-info">
-        <span>
-          <MovieDetail />
-        </span>
-      </div>
-    </div>
+    </Router>
   );
 };
 
